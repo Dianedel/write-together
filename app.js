f13f9c9595dc26b113a796bf89f424444b461792
Diane de Lassence <dianedelassence@gmail.com>
2018-07-03 09:51:50 +0200

    done

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(require('node-sass-middleware')({
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -53,6 +53,9 @@ app.locals.title = 'Express - Generated with IronGenerator';
 
 const index = require('./routes/index');
 app.use('/', index);
+
+const homeFr = require("./routes/french");
+app.use("/fr", homeFr);
 
 
 module.exports = app;
