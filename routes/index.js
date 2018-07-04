3b98914ba690b850c0dfe283cb7d506a15a74f36
Diane de Lassence <dianedelassence@gmail.com>
2018-07-04 19:29:08 +0200

    boutons auteurs log et routes

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,9 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
-// GET log in
-router.get("/login", (req, res, next) => {
-  res.render("auth-views/log-in");
+// GET log in USERS
+router.get("/login/user", (req, res, next) => {
+  res.render("auth-views/log-in-user");
   })
 
   router.post("/process-login", (req, res, next) => {
@@ -50,11 +50,50 @@ router.get("/login", (req, res, next) => {
     });
 });
 
+// GET log in AUTHORS
+router.get("/login/author", (req, res, next) => {
+    res.render("auth-views/log-in-author");
+    })
+
+    router.post("/process-login", (req, res, next) => {
+      const { email, loginPassword } = req.body;
+
+      // check the email by searching the database
+      Author.findOne({ email })
+      .then((authorDoc) => {
+          // "userDoc" will be falsy if we didn't find a user (wrong email)
+          if (!authorDoc) {
+              // req.flash("error", "Incorrect email.");
+              res.redirect("/login");
+              return;  // return instead of else when there's a lot of code
+          }
+
+          //  we are ready to check the password if the email is okay
+          const { encryptedPassword } = authorDoc;
+          // "compareSync()" will return false if the password is wrong
+          if (!bcrypt.compareSync(loginPassword, encryptedPassword) ){
+              res.redirect("/login");
+              return;
+          }
+
+          // we are ready to LOG THEM IN if we get here (password was okay too)
+          // "req.login()" is a Passport method for logging in a user
+          // (behind the scenes, in calls the "passport.serialize()" function)
+          req.login(authorDoc, () => {
+              // req.flash("success", "You successfully signed up");
+              res.redirect("/fr");
+          });
+      })
+      .catch((err) => {
+          next(err);
+      });
+  });
 
 
-// GET sign up
-router.get("/signup", (req, res, next) => {
-  res.render("auth-views/sign-up");
+
+// GET sign up USERS
+router.get("/signup/user", (req, res, next) => {
+  res.render("auth-views/sign-up-user");
 })
 
 router.post("/process-signup", (req, res, next) => {
@@ -82,6 +121,35 @@ router.post("/process-signup", (req, res, next) => {
   });
 });
 
+// GET sign up AUTHORS
+router.get("/signup/author", (req, res, next) => {
+    res.render("auth-views/sign-up-author");
+  })
+
+  router.post("/process-signup", (req, res, next) => {
+    const { firstName, lastName, description, email, originalPassword } = req.body;
+
+    // Password can't be blank and requires a number
+    if (originalPassword === "" || originalPassword.match(/[0-9]/) === null) {
+        //
+      //   req.flash("", "")
+        res.redirect("/signup");
+        return; // return instead of else when there's a lot of code
+    }
+
+    // we are ready to save the user if we get here
+    const encryptedPassword = bcrypt.hashSync(originalPassword, 10);
+
+    Author.create({ firstName, lastName, description, email, encryptedPassword })
+    .then((authorDoc) => {
+        //
+      //   req.flash("success", "....")
+        res.redirect("/fr");
+    })
+    .catch((err) => {
+        next(err);
+    });
+  });
 
 // log out
 router.get("/logout", (req, res, next) => {
